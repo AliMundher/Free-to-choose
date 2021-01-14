7b14164da53e40382c29ad3b2f5bb5f5ab2ef724
roustamkhaydarov <roustamkhaydarov@gmail.com>
2021-01-14 12:47:39 +0100

    added archives+images

---
+++ archives.php
@@ -0,0 +1,57 @@
+<?php get_header() ?>
+
+
+<!-- MAIN WRAP -->
+<div class="main__wrap d-flex justify-content-center">
+
+  <!-- INNER WRAP -->
+  <div class="inner_wrap">
+    
+  
+  
+  
+  </div>
+  <!--END INNER WRAP -->
+</div>
+<!-- END MAIN WRAP-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer()?>
