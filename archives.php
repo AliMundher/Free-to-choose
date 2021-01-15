89d71a0cedd4db6e5e615ea07137750c5af8e588
roustamkhaydarov <roustamkhaydarov@gmail.com>
2021-01-15 15:56:20 +0100

    addedsinglerecipe page

--- archives.php
+++ archives.php
@@ -1,57 +1,276 @@
 <?php get_header() ?>
 
-
 <!-- MAIN WRAP -->
-<div class="main__wrap d-flex justify-content-center">
+  <div class="main__wrap d-flex justify-content-center bg-light">
+    <!--INNER WRAP -->
+    <div class="inner__wrap my-5 d-flex flex-column justify-content-center">
 
-  <!-- INNER WRAP -->
-  <div class="inner_wrap">
-    
-  
-  
-  
+      <!--ARCHIVES TAGS -->
+      <div class="row d-flex my-4">
+
+        <div class="archive__tag mx-4">
+          <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+          <span class="tag__title"><strong>original's dev restaurant</strong></span>
+        </div>
+
+        <div class="archive__tag mx-4">
+          <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+          <span class="tag__title"><strong>local food</strong></span>
+        </div>
+
+        <div class="archive__tag mx-4">
+          <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+          <span class="tag__title"><strong>spicy</strong></span>
+        </div>
+        <div class="archive__tag mx-4">
+          <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+          <span class="tag__title"><strong>latino</strong></span>
+        </div>
+        <div class="archive__tag mx-4">
+          <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+          <span class="tag__title"><strong>italian</strong></span>
+        </div>
+      </div>
+      <!-- END ARCHIVES TAGS -->
+
+      <!-- ARCHIVES CARDS WRAP -->
+      <div class="container d-flex flex-column align-self-center mx-0">
+
+        <!-- ARCHIVE TEXT LEFT -->
+        <div class="archive row d-flex bg-white flex-row-reverse my-4">
+        
+          <!-- IMG -->
+          <div class="archive__image col-md-7 col-12 p-0 m-0">
+            <img src="assets/images/salad.png" class="img-fluid my-0">
+          </div>
+          <!-- END IMG -->
+        
+          <!-- TEXT -->
+          <div class="archive__info d-flex flex-column text-center align-content-center col-md-5 col-12  p-3 pt-4">
+        
+            <div class="archive__date d-flex justify-content-center mb-2">
+              <img src="assets/svg/time-clock.svg" height="10px" width="10px" class="archive__clock mx-1">
+              <span class="text-black-50">
+                11 MARCH 2020
+              </span>
+            </div>
+        
+            <div class="archive__tag my-2">
+              <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+              <span class="tag__title"><strong>local food</strong></span>
+            </div>
+        
+            <div class="archive__title my-2">
+              <span class="text-black"><strong>The Salade Liégoise</strong></span>
+            </div>
+        
+            <div class="archive__description my-4">
+              <span class="archive__info">Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+                chinum chinum grabum</span>
+            </div>
+        
+            <div class="readmore mt-2">
+              <span class="btn border px-4 bg-dark"><strong class="text-white">Read More</strong></span>
+            </div>
+        
+          </div>
+          <!-- END TEXT -->
+        
+        </div>
+        <!-- END LEFT -->
+
+
+        <!-- ARCHIVE TEXT RIGHT -->
+        <div class="archive row d-flex bg-white my-4">
+        
+          <!-- IMG -->
+          <div class="archive__image col-md-7 col-12 p-0 m-0">
+            <img src="assets/images/chickenwings.png" class="img-fluid">
+          </div>
+          <!-- END IMG -->
+
+          <!-- TEXT -->
+          <div class="archive__info d-flex flex-column text-center align-content-center col-md-5 col-12  p-3 pt-4">
+        
+            <div class="archive__date d-flex justify-content-center mb-2">
+              <img src="assets/svg/time-clock.svg" height="10px" width="10px" class="archive__clock mx-1">
+              <span class="text-black-50">
+                24 FEBRUARY 2020
+              </span>
+            </div>
+        
+            <div class="archive__tag my-2">
+              <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+              <span class="tag__title"><strong>spicy</strong></span>
+            </div>
+        
+            <div class="archive__title my-2">
+              <span class="text-black"><strong>chicken wings from hell</strong></span>
+            </div>
+        
+            <div class="archive__description my-4">
+              <span class="archive__info">Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+                chinum chinum grabum</span>
+            </div>
+        
+            <div class="readmore mt-2">
+              <span class="btn border px-4 bg-dark"><strong class="text-white">Read More</strong></span>
+            </div>
+        
+          </div>
+          <!-- END TEXT -->
+        
+        </div>
+        <!-- END RIGHT -->
+
+        <!-- ARCHIVE TEXT LEFT -->
+        <div class="archive row d-flex flex-row-reverse bg-white my-4">
+        
+          <!-- IMG -->
+          <div class="archive__image col-md-7 col-12 p-0 m-0">
+            <img src="assets/images/sandwich.png" class="img-fluid">
+          </div>
+          <!-- END IMG -->
+        
+          <!-- TEXT -->
+          <div class="archive__info d-flex flex-column text-center align-content-center col-md-5 col-12  p-3 pt-4">
+        
+            <div class="archive__date d-flex justify-content-center mb-2">
+              <img src="assets/svg/time-clock.svg" height="10px" width="10px" class="archive__clock mx-1">
+              <span class="text-black-50">
+                11 FEBRUARY 2020
+              </span>
+            </div>
+        
+            <div class="archive__tag my-2">
+              <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+              <span class="tag__title"><strong>original's dev restaurant</strong></span>
+            </div>
+        
+            <div class="archive__title my-2">
+              <span class="text-black"><strong>Midnight Sandwich</strong></span>
+            </div>
+        
+            <div class="archive__description my-4">
+              <span class="archive__info">Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+                chinum chinum grabum</span>
+            </div>
+        
+            <div class="readmore mt-2">
+              <span class="btn border px-4 bg-dark"><strong class="text-white">Read More</strong></span>
+            </div>
+        
+          </div>
+          <!-- END TEXT -->
+        
+        </div>
+        <!-- END LEFT -->
+        
+        
+        <!-- ARCHIVE TEXT RIGHT -->
+        <div class="archive row d-flex bg-white my-4">
+        
+          <!-- IMG -->
+          <div class="archive__image col-md-7 col-12 p-0 m-0">
+            <img src="assets/images/ceviche.png" class="img-fluid">
+          </div>
+          <!-- END IMG -->
+        
+          <!-- TEXT -->
+          <div class="archive__info d-flex flex-column text-center align-content-center col-md-5 col-12  p-3 pt-4">
+        
+            <div class="archive__date d-flex justify-content-center mb-2">
+              <img src="assets/svg/time-clock.svg" height="10px" width="10px" class="archive__clock mx-1">
+              <span class="text-black-50">
+                27 JANUARY 2020
+              </span>
+            </div>
+        
+            <div class="archive__tag my-2">
+              <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+              <span class="tag__title"><strong>latino</strong></span>
+            </div>
+        
+            <div class="archive__title my-2">
+              <span class="text-black"><strong>Ceviche</strong></span>
+            </div>
+        
+            <div class="archive__description my-4">
+              <span class="archive__info">Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+                chinum chinum grabum</span>
+            </div>
+        
+            <div class="readmore mt-2">
+              <span class="btn border px-4 bg-dark"><strong class="text-white">Read More</strong></span>
+            </div>
+        
+          </div>
+          <!-- END TEXT -->
+        
+        </div>
+        <!-- END RIGHT -->
+
+        <!-- ARCHIVE TEXT LEFT -->
+        <div class="archive row d-flex flex-row-reverse bg-white my-4">
+        
+          <!-- IMG -->
+          <div class="archive__image col-md-7 col-12 p-0 m-0">
+            <img src="assets/images/swordfish.png" class="img-fluid">
+          </div>
+          <!-- END IMG -->
+        
+          <!-- TEXT -->
+          <div class="archive__info d-flex flex-column text-center align-content-center col-md-5 col-12  p-3 pt-4">
+        
+            <div class="archive__date d-flex justify-content-center mb-2">
+              <img src="assets/svg/time-clock.svg" height="10px" width="10px" class="archive__clock mx-1">
+              <span class="text-black-50">
+                12 JANUARY 2020
+              </span>
+            </div>
+        
+            <div class="archive__tag my-2">
+              <img src="assets/svg/cutelry.svg" width="12px" height="12px">
+              <span class="tag__title"><strong>orignal's dev restaurant</strong></span>
+            </div>
+        
+            <div class="archive__title my-2">
+              <span class="text-black"><strong>The sword of king</strong></span>
+            </div>
+        
+            <div class="archive__description my-4">
+              <span class="archive__info">Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+                chinum chinum grabum</span>
+            </div>
+        
+            <div class="readmore mt-2">
+              <span class="btn border px-4 bg-dark"><strong class="text-white">Read More</strong></span>
+            </div>
+        
+          </div>
+          <!-- END TEXT -->
+        
+        </div>
+        <!-- END LEFT -->
+      
+      </div>
+      <!-- END ARCHIVES CARDS-->
+
+      <!-- PAGIN-->
+      <div class="row pagination d-flex justify-content-center my-5">
+        <div class="archives__pages">
+          <i class="fas fa-arrow-left mx-3"></i>
+          <div class="btn border border-rounded p-1 bg-white shadow-sm"><span class="p-1 mx-2 text-black-50">1</span></div>
+          <div class="btn border border-rounded p-1 bg-white shadow-sm"><span class="p-1 mx-2 text-black-50"><strong>2</strong></span></div>
+          <div class="btn border border-rounded p-1 bg-white shadow-sm"><span class="p-1 mx-2 text-black-50"><strong>3</strong></span></div>
+          <div class="btn border border-rounded p-1 bg-white shadow-sm"><span class="p-1 mx-2 text-black-50"><strong>4</strong></span></div>
+          <i class="fas fa-arrow-right mx-3"></i>
+        </div>
+      </div>
+      <!-- END PAGIN -->
+    </div>
+    <!-- END INNER WRAP -->
   </div>
-  <!--END INNER WRAP -->
-</div>
-<!-- END MAIN WRAP-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  <!-- END MAIN WRAP -->
 
 <?php get_footer()?>
---
+++ singlerecipe.php
@@ -0,0 +1,178 @@
+<?php get_header() ?>
+ <!-- MAIN WRAP -->
+  <div class="main__wrap d-flex justify-content-center">
+    <!--INNER WRAP -->
+    <div class="inner__wrap my-5 d-flex flex-column justify-content-center">
+
+      <!-- NAV + INFO -->
+      <div class="row nav__info align-self-center bg-danger">
+
+        <div class="d-flex justify-content-start ml-0 col-md-2 col-12">
+        <i class="fas fa-arrow-left mx-3 text-white pt-1"></i>
+        <span class="text-white"><strong>Retour</strong></span>
+        </div>
+
+        <div class="recipe__tag d-flex align-items-center mt-0 col-md-10 col-12">
+          <span class="recipe__date text-white pl-3"><strong>15 MARCH 2020</strong></span>
+          <div class="tag align-self-end ml-auto">
+            <img src="assets/svg/cutelry.svg" width="12px" height="12px" class="tag__cutelry mx-1">
+            <span class="tag__title"><strong>local food</strong></span>
+          </div>
+        </div>
+
+      </div>
+      <!-- END NAV + INFO -->
+
+      <!-- RECIPE WRAP -->
+      <div class="container recipe__wrap d-flex flex-column align-self-center mx-0">
+
+        <h3 class=" row recipe__title my-5 no-gutters">The Salade Liégoise</h3>
+
+        <!-- RECIPE DESCRIPTION -->
+        <div class="row recipe__description no-gutters mb-4">
+          <span>
+            Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+            chinum chinum grabum
+          </span>
+        </div>
+        <!-- END RECIPE DESCRIPTION -->
+
+        <!-- RECIPE IMAGE-->
+        <div class="row align-self-center image__wrap">
+          <img src="assets/images/bigsalad.png" class="archive__image">
+        </div>
+        <!-- END RECIPE IMAGE-->
+
+        <!-- INGREDIENTS -->
+        <div class="ingredients_wrap row  mt-4 mb-5">
+          
+          <div class=" social__wrap col-1">
+            <a class="social social__fb d-flex justify-content-center align-items-center my-1"><i class="fab fa-facebook-f"></i></a>
+            <a class="social social__twitter d-flex justify-content-center align-items-center my-1"><i class="fab fa-twitter"></i></a>
+            <a class="social social__instagram d-flex justify-content-center align-items-center my-1"><i class="fab fa-instagram"></i></a>
+            <a class="social social__mail d-flex justify-content-center align-items-center my-1" style="text-decoration: none"><i class="fas fa-envelope"></i></a>
+          </div>
+
+          <div class="col-11 ingredients__wrap">
+            <h5><strong>Ingredients</strong></h5>
+            <div class="my-4">For 4 people - preparation 30 min :</div>
+            <ul class="px-3">
+              <li><div class="ml-5">pomme de terre à chair ferme : 500 g</div></li>
+              <li><div class="ml-5">haricots verts : 800 g</div></li>
+              <li><div class="ml-5">oignon : 1</div></li>
+              <li><div class="ml-5">allumettes de lardons fumés : 250 g</div></li>
+              <li><div class="ml-5">beurre : 1 c. à soupe</div></li>
+              <li><div class="ml-5">vinaigre balsamique : 2 c. à soupe</div></li>
+            </ul>
+
+          </div>
+
+        </div>
+        <!-- END INGREDIENTS -->
+
+        <!-- INSTRUCTIONS TITLE -->
+        <div class="row">
+          <div class="col-1">
+
+          </div>
+          <div class="col-11">
+            <h5><strong>Instructions</strong></h5>
+          </div>
+        </div>
+        <!-- END INSTRUCTIONS TITLE -->
+
+        <!-- INSTRUCTIONS -->
+        <div class="instructions__wrap row my-3">
+          
+          <div class="col-1 d-flex justify-content-end align-items-start">
+            <div class="culinary__steps d-flex justify-content-center align-items-center text-white"><strong>1</strong></div>
+          </div>
+
+          <div class="col-11 text-black-50">
+            <span>Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+            chinum chinum grabum</span>
+          </div>
+
+        </div>
+        <!-- END INSTRUCTIONS-->
+
+        <!-- INSTRUCTIONS -->
+        <div class="instructions__wrap row my-3">
+        
+          <div class="col-1 d-flex justify-content-end align-items-start">
+            <div class="culinary__steps d-flex justify-content-center align-items-center text-white"><strong>2</strong></div>
+          </div>
+        
+          <div class="col-11 text-black-50">
+            <span>Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+              chinum chinum grabum</span>
+          </div>
+        
+        </div>
+        <!-- END INSTRUCTIONS-->
+
+        <!-- RECIPE IMAGE-->
+        <div class="row my-2">
+          <div class="col-1"></div>
+          <div class="col-11 m-0"><img src="assets/images/midnightsandwich.png" class="recipe__image" height="100%" width="93.5%"></div>
+        </div>
+        <!-- END RECIPE IMAGE-->
+
+        <!-- INSTRUCTIONS -->
+        <div class="instructions__wrap row my-3">
+        
+          <div class="col-1 d-flex justify-content-end align-items-start">
+            <div class="culinary__steps d-flex justify-content-center align-items-center text-white"><strong>3</strong></div>
+          </div>
+        
+          <div class="col-11 text-black-50">
+            <span>Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+              chinum chinum grabum</span>
+          </div>
+        
+        </div>
+        <!-- END INSTRUCTIONS-->
+
+        <!-- INSTRUCTIONS -->
+        <div class="instructions__wrap row my-3">
+        
+          <div class="col-1 d-flex justify-content-end align-items-start">
+            <div class="culinary__steps d-flex justify-content-center align-items-center text-white"><strong>4</strong></div>
+          </div>
+        
+          <div class="col-11 text-black-50">
+            <span>Loremp Ipsum machinum trucum bidulum perlinpinpum trumpum covidum chinum
+              chinum chinum grabum</span>
+          </div>
+        
+        </div>
+        <!-- END INSTRUCTIONS-->
+
+        <!-- SOCIAL ROW -->
+        <div class="row my-5">
+          
+          <div class="col-1"></div>
+
+          <div class="col-11 d-flex justify-content-end">
+
+            <div class="social__wrap2">
+              <a class="social social__fb d-flex justify-content-center align-items-center mx-1"><i class="fab fa-facebook-f"></i></a>
+              <a class="social social__twitter d-flex justify-content-center align-items-center mx-1"><i
+                  class="fab fa-twitter"></i></a>
+              <a class="social social__instagram d-flex justify-content-center align-items-center mx-1"><i
+                  class="fab fa-instagram"></i></a>
+              <a class="social social__mail d-flex justify-content-center align-items-center mx-1"><i class="fas fa-envelope"></i></a>
+            </div>
+
+          </div>
+
+        </div>
+        <!-- END SOCIAL ROW -->
+      </div>
+      <!-- END RECIPE WRAP -->
+    </div>
+    <!-- END INNER WRAP-->
+  </div>
+  <!-- END MAIN WRAP-->
+
+<?php get_footer() ?>
