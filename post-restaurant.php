80a4e8c851e667585cb261e1228c57e830e4b70b
rapahel colson <rapahael.kasese@gmailcom>
2021-01-18 11:08:15 +0100

    post restaurant terminer

---
+++ post-restaurant.php
@@ -0,0 +1,164 @@
+<?php get_header() ?>
+
+    <!-- header -->
+    <div class="contenair header-top">
+        <div class="overlay"></div>
+
+        <div class="container container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light ">
+              <div class="container-fluid">
+                <h3 class="text-light">DEV RESTAURANT</h3>
+
+                <div class="navbar-droite fluid d-flex flex-wrap">    
+                    <a href="#" class="text-light mr-4">Icon</a>
+                    <a href="#" class="text-light mr-4">Ours</a>
+                    <a href="#" class="text-light mr-4">Menu</a>
+                    <a href="#" class="text-light mr-4">Recipes</a>
+                    <a href="#" class="text-light mr-4">Reservations</a>
+                    <a href="#" class="text-dark mr-4 bg-white p-1">ORDER</a>
+                    <a href="#" class="text-light mr-4">Panier</a>
+                </div>
+
+              </div>
+
+            </nav>
+          </div>
+          <div class="container block-text mt-5">
+            <p class="text-reader">Our original restaurant</p>
+            <h1>THE CHEF'S <br> CAFETERIA</h1>
+          </div>
+          
+    </div>
+    <!-- header fin-->
+
+    <!-- images hachures -->
+    <div class="fluid rotate_noise">
+        <img src="../images/hachures-blanches.png" class="img-fluid" alt="hachures">
+    </div>
+    <!-- images hachures fin-->
+   
+    <div class="section-presention contenair">
+        <p class="text-center text-dark">Welcome in Liège <br>  <strong>PRESENTATION</strong></p>
+    </div>
+
+     <!-- block-menu1 -->
+    <div class="block-menu contenair d-flex justify-content-center mb-5">
+
+        <div class="block-img col-4"></div>
+        
+        <div class="block-text bg-light col-4 border border-Secondary ">
+            <div class="text-center mt-5">Our original restaurant</div>
+                <div class="text-center text-justify font-weight-bold  h2 mt-3">The Chef's cafeteria</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+        </div>
+    </div>
+    <!-- block-menu1 fin-->
+    <!-- block-menu2 -->
+    <div class="block-menu contenair d-flex justify-content-center">
+
+        <div class="block-text2 bg-light col-4 border border-Secondary">
+            <div class="contenair ">
+                <div class="text-center mt-5 ">Our original restaurant</div>
+                <div class="text-center text-justify font-weight-bold  h2 mt-3">The Chef's cafeteria</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                <div class="text-center text-justify mt-3">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                
+            </div>
+        </div>
+
+        <div class="block-img2 col-4"></div>
+    </div>
+    <!-- block-menu2 fin-->
+
+    <div class="bg-warnig carte-google-maps 
+    text-center">
+    <div class="overlay"></div>
+        <p class="mb-2">Find Us</p>
+        <h3 class="font-weight-bold mb-5">LOCATION</h3>
+
+        <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.5475660675183!2d5.578015815695458!3d50.63552108189324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0fa0751102ab7%3A0x2242ad6ed846472e!2sMediacit%C3%A9!5e0!3m2!1sfr!2sbe!4v1610637015442!5m2!1sfr!2sbe" width="100%" height="500px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+
+
+    <div class="discover-menu container d-flex mb-5">
+        <img class="img img-responsive" src="../images/Dev Restaurant - Post-type Restaurant - Desktop (1).jpg" alt="">
+        <div class="container-fluid text-center">
+            <p class="font-weight-bold mt-3 mb-5">LET'S DISCOVER FOOD</p>
+            <h3 class="mb-5">DISCOVER OUR MENU</h3>
+            <p class="mt-5">for those whith pure food indulgence in mind comme and sate your desires</p>
+            <p class="">with our ever changing internationally and seasonally inspiresd small plate. we</p>
+            <p class="mb-5">Love food. lots of different food just like you.</p>
+            <p class="btn btn-dark mt-5"><a href="https://.www.google.com"></a>View the full Menu</p>
+        </div>
+    </div>
+    
+
+    <div class="text-center mt-5">
+        <h3>Last updated</h3>
+        <h3>RECIPES BLOG</h3>
+    </div>
+
+    <div class="recipe-blog container card-group d-flex ">
+        
+        <div class="recipe-blog-card card mr-3 ">
+          <img src="https://images.unsplash.com/photo-1597699401474-e8714c1b7879?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text"><small class="text-muted">OCTOBER 4, 2018</small></p>
+            <h5 class="card-title">Fluffy Japanese Pancakes:</h5>
+            <p class="card-text">This is a wider card supporting</p>
+            <p class="card-text">This is a wider with supporting</p>
+            <p class="card-text">This is a card  with supporting</p>
+            <p class="card-text text-right font-weigth-bold btn btn-dark">READ MORE</p>
+            
+          </div>
+        </div>
+        <div class="recipe-blog-card card mr-3">
+          <img src="https://images.unsplash.com/photo-1549892898-79ac97b31fb2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text"><small class="text-muted">OCTOBER 4, 2018</small></p>
+            <h5 class="card-title">Sunday Brunch:Eggslut's</h5>
+            <p class="card-text">This is a wider card supporting</p>
+            <p class="card-text">This is a wider with supporting</p>
+            <p class="card-text">This is a card  with supporting</p>
+            <p class="card-text text-right font-weigth-bold btn btn-dark">READ MORE</p>
+            
+          </div>
+        </div>
+        <div class="recipe-blog-card card mr-3">
+          <img src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text"><small class="text-muted">OCTOBER 4, 2018</small></p>
+            <h5 class="card-title">Copycat Panda Express</h5>
+            <p class="card-text">This is a wider card supporting</p>
+            <p class="card-text">This is a wider with supporting</p>
+            <p class="card-text">This is a card  with supporting</p>
+            <p class="card-text text-right font-weigth-bold btn btn-dark">READ MORE</p>
+            
+          </div>
+        </div>
+        <div class="recipe-blog-card card mr-3">
+          <img src="https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text"><small class="text-muted">OCTOBER 4, 2018</small></p>
+            <h5 class="card-title">Delfina's Secret Pork Sugo</h5>
+            <p class="card-text">This is a wider card supporting</p>
+            <p class="card-text">This is a wider with supporting</p>
+            <p class="card-text">This is a card  with supporting</p>
+            <p class="card-text text-right font-weigth-bold btn btn-dark">READ MORE</p>
+            
+          </div>
+        </div>
+      </div>
+      
+      <?php get_footer() ?>
